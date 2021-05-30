5b70edf89945a692e49ecea5f62ce45896feb6cc
snappyyw <snippymr@yandex.ru>
2021-05-30 23:22:13 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 
-import './scss/app.scss';
+import './scss/index.css';
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './App.js';
